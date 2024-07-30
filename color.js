94dbb23012b6e65da0c8746346ee42f15918ee81
sir-serge <irkzsrg@gmail.com>
2024-07-30 11:25:53 +0200

    creat color.js

---
+++ color.js
@@ -0,0 +1,8 @@
+const colorSet=["#52327A","#D6C7E9","#C7E9D6","blue","pink","skyblue"];
+const clicking=document.getElementById("clicking");
+// const fliping=document.querySelector(".fliping");
+const color=document.getElementById("fliping") 
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colorSet.length);
+    }
