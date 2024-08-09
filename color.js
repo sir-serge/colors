58297edd1bef04a703b3694e1d50dc64c78c19c7
sir-serge <irkzsrg@gmail.com>
2024-08-09 11:37:15 +0200

    finish color.js

--- color.js
+++ color.js
@@ -2,7 +2,12 @@ const colorSet=["#52327A","#D6C7E9","#C7E9D6","blue","pink","skyblue"];
 const clicking=document.getElementById("clicking");
 // const fliping=document.querySelector(".fliping");
 const color=document.getElementById("fliping") 
-
+clicking.addEventListener("click" ,function (){
+    const randomNumber= getRandomNumber();
+    document.body.style.backgroundColor=colorSet[randomNumber];
+    color.textContent=colorSet[randomNumber];
+    // console.log("working")
+});
 function getRandomNumber(){
     return Math.floor(Math.random()*colorSet.length);
     }
